810e21ecb62065914db9dbf7671ea17fb3925b5d
hieunguyen097 <115157456+hieunguyen097@users.noreply.github.com>
2022-12-03 22:25:53 +0700

    Update staff.js

--- js/staff.js
+++ js/staff.js
@@ -1,1 +1,44 @@
 
+function Staff(
+  account,
+  fullName,
+  email,
+  password,
+  datepicker,
+  wage,
+  office,
+  workTime
+) {
+  this.account = account;
+  this.fullName = fullName;
+  this.email = email;
+  this.password = password;
+  this.datepicker = datepicker;
+  this.wage = wage;
+  this.office = office;
+  this.workTime = workTime;
+
+  this.totalSalary = function () {
+    if (this.office === "Sếp") {
+      return this.wage * 3;
+    } else if (this.office === "Trưởng phòng") {
+      return this.wage * 2;
+    } else {
+      return this.wage;
+    }
+  };
+
+  this.rating = function(){
+    if((workTime * 1) >= 192){
+        return "Nhân viên xuất sắc"
+    }else if((workTime * 1) < 192 || (workTime * 1) >= 176){
+        return "Nhân viên giỏi"
+    }else if((workTime * 1) < 176 || (workTime * 1) >= 160){
+        return "Nhân viên khá"
+    }else{
+        return "Nhân viên hơi lười"
+    }
+  }
+
+
+}
